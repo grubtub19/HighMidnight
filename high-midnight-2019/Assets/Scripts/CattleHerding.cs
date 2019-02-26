02801c9e8eb3314f4ce4c5f093b63bfffb1eddd8
= <ptm020@utulsa.edu>
2019-02-26 13:40:51 -0600

    Added herd prefab and improved movement scripts

---
+++ high-midnight-2019/Assets/Scripts/CattleHerding.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9ad002926f0f1c19fd9d9b152462ff96c5f8b33c5248c23dc12b2a627f6b9ea6
+size 2975
---
+++ high-midnight-2019/Assets/Scripts/HerdController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3018e91311b85a30c3cbd1496ca761a1ce2eee34965797eec886cbe136a561ea
+size 1846
