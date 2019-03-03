dc14dcea7f5aa3e2dd916ed51ecbd9df87f18358
Peter Robe <probe191919@gmail.com>
2019-03-03 17:17:49 -0600

    Created hitbox and death animation

---
+++ high-midnight-2019/Assets/Whip_Crack.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f4a9579862eb9e8f48024ca6bee9f871413a0a5f5eb09e93a529448a8df510e1
+size 545
---
+++ high-midnight-2019/Assets/enemy_effects.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2b45607290016981fac44732021e6b8d9f5afc0dded0762f18532fcf80ea521f
+size 922
