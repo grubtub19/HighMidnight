b9e9fdff932c0eb074845fd7c3484c66aec03b33
Peter Robe <probe191919@gmail.com>
2019-03-03 18:33:44 -0600

    fixed sound. Might have fixed animations?

--- high-midnight-2019/Assets/Whip_Crack.cs
+++ high-midnight-2019/Assets/Whip_Crack.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8dd9a883d9ae2a7242bc8a9143f1159e8173d504f2f405b8ce7b2ddf4cf5df44
-size 672
+oid sha256:546353cb15553ffcc4a6eab3181785b65533d83a41c684d1b17e2781303b4688
+size 717
---
+++ high-midnight-2019/Assets/whip_sound.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:61b9639433a09889effc4f38bd4051b3a480942177a7f65285eb3aba0aa939db
+size 377
