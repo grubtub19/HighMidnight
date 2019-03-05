bf53cd7995f88d314504b7558947a24f3c204255
Peter Robe <probe191919@gmail.com>
2019-03-05 13:30:17 -0600

    Fixed death animation and whip sound effect

--- high-midnight-2019/Assets/Whip_Crack.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:546353cb15553ffcc4a6eab3181785b65533d83a41c684d1b17e2781303b4688
-size 717
--- high-midnight-2019/Assets/enemy_effects.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:2b45607290016981fac44732021e6b8d9f5afc0dded0762f18532fcf80ea521f
-size 922
