0816ca139c7fa885d777b35d09a4e4b3016a6b3a
Peter Robe <probe191919@gmail.com>
2019-03-08 14:50:02 -0600

    Added WhipTest

--- high-midnight-2019/Assets/Tests/PlayMode/WhipTest.cs
+++ high-midnight-2019/Assets/Tests/PlayMode/WhipTest.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a38f298346752021eee05dcb6cf8e63d66ad23a2b2a0d9a2759680991556a2f9
-size 1611
+oid sha256:39c819b984c5f5e77b931ce61ea4992379825fa21ce023840f7eda0d53f9dc7d
+size 1624
