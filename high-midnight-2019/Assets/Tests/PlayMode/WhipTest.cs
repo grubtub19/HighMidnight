035ad68833d79188262d5614838cdf1a50cf2c07
Peter Robe <probe191919@gmail.com>
2019-03-07 19:45:35 -0600

    Added Whip and Enemy Test Scenes

--- high-midnight-2019/Assets/Tests/PlayMode/WhipTest.cs
+++ high-midnight-2019/Assets/Tests/PlayMode/WhipTest.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c747b6812f1430c0a93f77ac2ecf4096f7921fb26c7097331753088bdb36fd47
-size 1507
+oid sha256:a38f298346752021eee05dcb6cf8e63d66ad23a2b2a0d9a2759680991556a2f9
+size 1611
