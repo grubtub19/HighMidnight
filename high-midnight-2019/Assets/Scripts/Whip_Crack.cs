dfe33e79da6e0aaaac10bb311547cab3c61b7596
Lab Computer <probe191919@gmail.com>
2019-03-08 20:44:43 -0600

    Improved Whip and Fixed

--- high-midnight-2019/Assets/Scripts/Whip_Crack.cs
+++ high-midnight-2019/Assets/Scripts/Whip_Crack.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:04fa020850c6e0047dfd60a1e2236d7e70c932c0f9b9f32ea3382ba66d593c3e
-size 822
+oid sha256:6e6c4e42547d8fd75eef2fa13de404601ed13eeda4f163a2c919ed8f1621c9ec
+size 880
