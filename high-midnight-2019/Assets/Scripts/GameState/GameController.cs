eea0fd2539b52be31d3b16e2259a60850af5a257
= <ptm020@utulsa.edu>
2019-03-24 12:52:38 -0500

    Added var to track remaining cow count

--- high-midnight-2019/Assets/Scripts/GameState/GameController.cs
+++ high-midnight-2019/Assets/Scripts/GameState/GameController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce93bb882ff9bfaaa4cbbe50cc5a234bf7b1ce8349045dad35aeb7212bc1080b
-size 554
+oid sha256:3f7509b19ba83d26a5c8a8c45a10ac59e85c1494e0cd620e6720e25b9477458b
+size 604
