35a1a994deff24433f3054c3b8d31348e2ce6c79
ptm020 <ptm020@utulsa.edu>
2019-04-04 16:17:23 -0500

    Player Movement script now rotates player, moves relative to horse orientation

--- high-midnight-2019/Assets/Scripts/Player_Move.cs
+++ high-midnight-2019/Assets/Scripts/Player_Move.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:651b2cd6a31f84f1c541d5ffb3a0e83c6b724ce1e9b92b6b7255282f391ed92e
-size 1824
+oid sha256:fe893da267f215f9354969ca54c29126e48f7e7d1fac619aa115d1667cdc61ec
+size 2433
