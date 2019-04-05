1ee33d8e75a565b2a9e56f3be84ad64ce1351a5c
ptm020 <ptm020@utulsa.edu>
2019-04-04 21:25:27 -0500

    Modified GameController to factor saved cow count into final score

--- high-midnight-2019/Assets/Scripts/GameState/GameController.cs
+++ high-midnight-2019/Assets/Scripts/GameState/GameController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:efb2d5f1b3a104b3c8dda6cb1dfe3ca241482fdadda931f4dbab26be85ec8868
-size 3733
+oid sha256:027e49412513db862563cf11f2f7f036e5d641fcdeadf0df068db3b43a912c3e
+size 3766
