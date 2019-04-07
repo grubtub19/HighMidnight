333957d1ef013f7fed08f326dcb195b1e30a6f4c
ptm020 <ptm020@utulsa.edu>
2019-04-07 12:37:13 -0500

    modified scripts to pause properly

--- high-midnight-2019/Assets/High Midnight/Scripts/Player_Move.cs
+++ high-midnight-2019/Assets/High Midnight/Scripts/Player_Move.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:883261986b23ed95aeaf004b5224008b299a2c95bd53a3275c042ad8e724eab6
-size 2448
+oid sha256:a76a98081cfe7825e8e0ae30abde9c56474a0705cd54c8f40b0a4d00b8fbb790
+size 2930
